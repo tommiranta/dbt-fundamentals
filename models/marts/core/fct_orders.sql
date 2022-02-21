29dde2fd2f08c538116547e5d8809e6fdcd22587
Tommi <tommi.ranta@leadfeeder.com>
2022-02-21 17:08:13 +0000

    source and models

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,32 @@
+with customers as (
+
+    select * from {{ ref('stg_customers')}}
+
+),
+
+orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+payments as (
+
+    select * from {{ ref('stg_payments') }}
+
+),
+
+order_payments as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+
+        coalesce(payments.amount, 0) as amount
+
+    from orders
+    left join payments using (order_id)
+)
+
+select * from order_payments
