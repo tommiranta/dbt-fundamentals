b79fd5eba06c488c485b52c172f9d4d9014dd7b0
tommi.ranta@leadfeeder.com <tommi.ranta@leadfeeder.com>
2022-02-28 09:05:37 +0000

    analyses and seeds

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -10,6 +10,10 @@ orders as (
 
 ),
 
+employees as (
+
+    select * from {{ ref('employees') }}
+),
 
 customer_orders as (
 
@@ -33,6 +37,7 @@ final as (
         customers.customer_id,
         customers.first_name,
         customers.last_name,
+        employees.employee_id is not null as is_employee,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
@@ -41,6 +46,7 @@ final as (
     from customers
 
     left join customer_orders using (customer_id)
+    left join employees using (customer_id)
 
 )
 
